2a411934fd8d94385944eaea9bcc33601f64f31d
Ricardo Pabón <rpabon257@gmail.com>
2020-12-25 21:15:14 +0100

    Minor fixes

--- src/components/CollectionItems/ToggleItems.tsx
+++ src/components/CollectionItems/ToggleItems.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { FaChevronDown } from 'react-icons/fa';
+// import { FaChevronDown } from 'react-icons/fa';
 import css from './CollectionItems.module.scss';
 
-export function ToggleItems() {
+export function ToggleItems({ onClick }: ToggleItemsProps) {
   return (
     <h2 className={css.title}>
-      <button className={css.toggleList}>
+      {/* <button className={css.toggleList} onClick={onClick}>
         <FaChevronDown />
-      </button>
+      </button> */}
       Items
     </h2>
   );
+}
+
+interface ToggleItemsProps {
+  onClick?(): void;
 }
